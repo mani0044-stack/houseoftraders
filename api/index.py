dbcef6bd8c862fb4f74a694eaae7d0bcad24a099
Mani-social <mankirat.singh382@gmail.com>
2026-09-24 17:00:41 +0530

    m

--- api/index.py
+++ api/index.py
@@ -1,4 +1,10 @@
+import os
+import sys
+
+# Add project root directory to Python path for Vercel Serverless runtime
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from backend.main import app
 
-# Export FastAPI instance for Vercel Serverless Functions
+# Export FastAPI app for Vercel Serverless Functions
 app = app
